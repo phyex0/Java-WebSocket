22c8c5cd772c3899cfe012047dd944edcc7f8aad
Halit Burak Yeşildal <48446413+phyex0@users.noreply.github.com>
2024-10-03 17:06:23 +0300

    Add files via upload

---
+++ socket-manager-scalable/main/java/com/phyex0/socketmanagerscalable/SocketManagerScalableApplication.java
@@ -0,0 +1,13 @@
+package com.phyex0.socketmanagerscalable;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SocketManagerScalableApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SocketManagerScalableApplication.class, args);
+    }
+
+}
